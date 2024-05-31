c1a98c8b2c61849b20b7f142983b0ee7cd617dfe
sadek1999 <sadekali1530@gmail.com>
2024-05-31 23:27:43 +0600

    set student interface

---
+++ src/moduls/student/student.interface.ts
@@ -0,0 +1,40 @@
+
+
+export type TGuardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContact: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContact: string;
+  };
+  
+  export type TLocalGuardian = {
+    name: string;
+    address: string;
+    contact: string;
+  };
+  
+  export type TUserName={
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  }
+  export type TStudent = {
+    id: string;
+    name: TUserName;
+  
+    gender: "male" | "female";
+    email: string;
+    dateOfBarth:string,
+    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  contactNO: string;
+    EmergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: TGuardian;
+    localGuardian:TLocalGuardian,
+    profileImg:string,
+    isActive:'active'|"inActive"
+  };
+  
